140e216c35d1ee11afb711df11374feda69e91fc
rajali <rajali1432@gmail.com>
2024-03-28 19:15:31 +0530

    match and project aggregation videos 5

--- routers/users.js
+++ routers/users.js
@@ -34,71 +34,71 @@ const router = require("express").Router()
 // router.get("/",async(req,res) =>{
 //     try {
 //         //! what is the average age of  users
-//         // const users = await User.aggregate([
-//         //     {
-//         //        $group:{
-//         //         _id:null,
-//         //         averageAge:{
-//         //             $avg:"$age"
-//         //         }
-//         //        }
-//         //     }
-//         // ])
+        // const users = await User.aggregate([
+        //     {
+        //        $group:{
+        //         _id:null,
+        //         averageAge:{
+        //             $avg:"$age"
+        //         }
+        //        }
+        //     }
+        // ])
 
 
 //         //! list the top 5/2 most common favorite fruits among user
 
-//         // const users = await User.aggregate([
-//         //     {
-//         //        $group:{
-//         //         _id:"$favoriteFruit",
-//         //          count:{
-//         //             $sum:1
-//         //          }
-//         //        },
+        // const users = await User.aggregate([
+        //     {
+        //        $group:{
+        //         _id:"$favoriteFruit",
+        //          count:{
+        //             $sum:1
+        //          }
+        //        },
 
-//         //     },
-//         //     {
-//         //         $sort:{
-//         //             count:-1
-//         //         }
-//         //     },
-//         //     {
-//         //         $limit:2
-//         //     }
-//         // ])
+        //     },
+        //     {
+        //         $sort:{
+        //             count:-1
+        //         }
+        //     },
+        //     {
+        //         $limit:2
+        //     }
+        // ])
 
 //         //! find the total no of male and female
-//         // const users = await User.aggregate([
-//         //     {
-//         //         $group:{
-//         //             _id:"$gender",
-//         //             genderCount:{
-//         //                 $sum:1
-//         //             },
-//         //         }
-//         //     }
-//         // ])
+        // const users = await User.aggregate([
+        //     {
+        //         $group:{
+        //             _id:"$gender",
+        //             genderCount:{
+        //                 $sum:1
+        //             },
+        //         }
+        //     }
+        // ])
 
 //         //! which country has the highest number of registered users
-//         // const users = await User.aggregate([
-//         //      {
-//         //         $group:{
-//         //             _id:"$company.location.country",
-//         //             userCount:{
-//         //                 $sum:1
-//         //             }
-//         //         }
-//         //      },
-//         //      {
-//         //         $sort:{
-//         //             userCount: -1
-//         //         }
-//         //      },
-//         //      {
-//         //         $limit:1
-//         //      }
-//         // ])
+        // const users = await User.aggregate([
+        //      {
+        //         $group:{
+        //             _id:"$company.location.country",
+        //             userCount:{
+        //                 $sum:1
+        //             }
+        //         }
+        //      },
+        //      {
+        //         $sort:{
+        //             userCount: -1
+        //         }
+        //      },
+        //      {
+        //         $limit:1
+        //      }
+        // ])
 
 
 //         //! list all the unique eye color present in collection with user name
@@ -122,13 +122,13 @@ const router = require("express").Router()
 
 //! videos 4
 
-router.get("/", async (req, res) => {
-    try {
+// router.get("/", async (req, res) => {
+//     try {
 
 
-        //!  What is the averge number of tags per users?
+//         //!  What is the averge number of tags per users?
 
-        //! method 1
+//         //! method 1
         // const users = await User.aggregate([
         //      {
         //         $unwind:"$tags"
@@ -153,34 +153,99 @@ router.get("/", async (req, res) => {
 
         // ])
 
-        //! method 2
-        const users = await User.aggregate([
-            {
-                $addFields:{
-                    numberOfTags:{
-                        $size:{$ifNull : ["$tags",[]]}
-                    }
-                }
-            },
+//         //! method 2
+//         const users = await User.aggregate([
+//             {
+//                 $addFields:{
+//                     numberOfTags:{
+//                         $size:{$ifNull : ["$tags",[]]}
+//                     }
+//                 }
+//             },
 
-            {
-                $group:{
-                    _id:null,
-                    averageofTags:{
-                        $avg:"$numberOfTags"
-                    }
-                }
-            }
+//             {
+//                 $group:{
+//                     _id:null,
+//                     averageofTags:{
+//                         $avg:"$numberOfTags"
+//                     }
+//                 }
+//             }
 
-        ])
+//         ])
 
-        return res.json({ length: users.length, users: users })
+//         return res.json({ length: users.length, users: users })
 
 
-    } catch (error) {
-        console.log('error', error)
-    }
-})
+//     } catch (error) {
+//         console.log('error', error)
+//     }
+// })
+
+
+
+
+//! videos 5
+//! Match and project pipeline
+
+// router.get("/", async (req, res) => {
+//     try {
+
+//         //! 1. How many users have 'enim' as one of their tags
+//         const users = await User.aggregate([
+//              {
+//                 $match:{
+//                     tags:"enim"
+//                 }
+//              },
+//              {
+//                 $count:"userWithtagsEnim"
+//              }
+//         ])
+
+//         //! 2. What are the names and age of user who are inactive and have 'velit' as their tag?
+
+//         const users = await User.aggregate([
+//              {
+//                 $match:{
+//                     isActive:false,
+//                     tags:'velit'
+//                 }
+//              },
+
+//              {
+//                 $project:{
+//                     name:1,
+//                     age:1
+//                 }
+//              }
+             
+//         ])
+
+
+//         //! 3. How many users have a phone number wtarting with '+1 (940)'?
+
+//         const users = await User.aggregate([
+//              {
+//                 $match:{
+//                     "company.phone": /^\+1 \(940\)/
+//                 }
+//              },
+
+//              {
+//                 $count:"userWithspecialPhone"
+//              }
+             
+//         ])
+
+
+//         return res.json({ length: users.length, users: users })
+
+
+//     } catch (error) {
+//         console.log('error', error)
+//     }
+// })
 
 
 module.exports = router
